e852393be6ae527efd8a1ad04e037dad9d97440b
4zv4l <simon.blacks.be@gmail.com>
2021-06-01 13:57:51 +0200

    add more little command

--- todo.go
+++ todo.go
@@ -10,22 +10,26 @@ import (
 	"time"
 )
 
-func help() { // show how to use todo
+var vers string
+
+func help() {
 	clear()
 	fmt.Println(`Usage :
 	write something to add in the list
 	write a number in the list to delete it
-	write -1 to end the program
-	`)
+	write s to save the todo
+	write all to clear the todo list
+	write -1 to save and end the program`)
+	fmt.Printf("\nVersion : %s\n", vers)
 	fmt.Print("Press Enter to continue...")
 	fmt.Scanln()
 }
 
-func remove(slice []string, s int) []string { // remove an element from a slice
+func remove(slice []string, s int) []string {
 	return append(slice[:s], slice[s+1:]...)
 }
 
-func clear() { // clear the screen depend on the os
+func clear() {
 	if runtime.GOOS == "windows" {
 		cmd := exec.Command("cmd", "/c", "cls")
 		cmd.Stdout = os.Stdout
@@ -37,10 +41,11 @@ func clear() { // clear the screen depend on the os
 	}
 }
 
+// load the todo file
 func load() []string {
-	path, _ := os.UserHomeDir() // make the path for the save file
+	path, _ := os.UserHomeDir() // get user home folder
 	path += "/.todo.mine"
-	f, err := os.OpenFile(path, os.O_RDONLY|os.O_CREATE, 0600) //open the file in read only
+	f, err := os.OpenFile(path, os.O_RDONLY|os.O_CREATE, 0600) //open the file in read only or create it
 	if err != nil {
 		print(err)
 		os.Exit(4)
@@ -48,16 +53,17 @@ func load() []string {
 	var buff []string
 	scan := bufio.NewScanner(f)
 	for scan.Scan() {
-		if scan.Text() == "" { // remove blank lines
-
+		if scan.Text() == "" {
+			// if enmpty line -> not include in todo list
 		} else {
-			buff = append(buff, scan.Text())
+			buff = append(buff, scan.Text()) // get the todo list
 		}
 	}
 	f.Close()
 	return buff
 }
 
+// save to the todo file
 func save(b []string) {
 	path, _ := os.UserHomeDir()
 	path += "/.todo.mine"
@@ -72,37 +78,54 @@ func save(b []string) {
 	}
 }
 
+func show_list(todo []string) {
+	for n, do := range todo {
+		fmt.Printf("  %d. %s\n", n+1, do)
+	}
+}
+
+func action_number(todo *[]string, scan *bufio.Scanner) {
+	num, _ := strconv.Atoi(scan.Text())
+	if num == -1 {
+		save(*todo)
+		os.Exit(0)
+	} else if num > len(*todo) || num <= 0 {
+		println("number not in the list...")
+		time.Sleep(1 * time.Second)
+	} else {
+		*todo = remove(*todo, num-1)
+
+	}
+}
+
+func action_str(todo *[]string, scan *bufio.Scanner) {
+	if scan.Text() == "h" {
+		help()
+	} else if scan.Text() == "s" {
+		save(*todo)
+	} else if scan.Text() == "all" {
+		*todo = nil
+		clear()
+	} else if scan.Text() == "" {
+
+	} else {
+		*todo = append(*todo, scan.Text())
+	}
+}
+
 func main() {
-	var todo []string = load() // load previous todo
+	var todo []string = load()
 	scan := bufio.NewScanner(os.Stdin)
 	clear()
 	for {
 		fmt.Println("To do (h for help):")
-		for n, do := range todo {
-			fmt.Printf("  %d. %s\n", n+1, do)
-		}
+		show_list(todo)
 		fmt.Print("> ")
 		scan.Scan()
-		if _, err := strconv.Atoi(scan.Text()); err == nil {
-			num, _ := strconv.Atoi(scan.Text())
-			if num == -1 {
-				save(todo)
-				os.Exit(0)
-			} else if num > len(todo) || num <= 0 {
-				println("number not in the list...")
-				time.Sleep(1 * time.Second)
-			} else {
-				todo = remove(todo, num-1)
-
-			}
-		} else {
-			if scan.Text() == "h" {
-				help()
-			} else if scan.Text() == "" {
-
-			} else {
-				todo = append(todo, scan.Text())
-			}
+		if _, err := strconv.Atoi(scan.Text()); err == nil { // if number
+			action_number(&todo, scan)
+		} else { // if string
+			action_str(&todo, scan)
 		}
 		clear()
 	}
